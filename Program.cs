6a08667ff7060bd608c18935b9d48b714754c778
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-04-04 10:32:58 +0200

    implementazione modifiche proposte in classe

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ namespace UnicamMath
 {
     class Program
     {
+        private static double LeggiInputUtente(string messaggio)
+        {
+            while (true)
+            {
+                Console.Write(messaggio);
+                string cateto1Testuale = Console.ReadLine();
+                if (double.TryParse(cateto1Testuale, out double cateto))
+                    return cateto;
+                else
+                    Console.WriteLine("Non hai digitato un numero corretto, riprova.\n");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Questo programma calcola l'ipotenusa e gli angoli interni fornendo la lunghezza di due cateti");
@@ -11,31 +23,9 @@ namespace UnicamMath
             double angolo1 = 90;
             double angolo2 = 0;
             double angolo3 = 0;
-            double cateto1, cateto2;
-            while (true)
-            {
-                Console.Write("Inserisci la lunghezza del primo cateto: ");
-                string cateto1Testuale = Console.ReadLine();
-                if (double.TryParse(cateto1Testuale, out cateto1))
-                    break;
-                else
-                {
-                    Console.WriteLine("Non hai digitato un numero corretto, riprova.\n");
-                    continue;
-                }
-            }
-            while (true)
-            {
-                Console.Write("Inserisci la lunghezza del secondo cateto: ");
-                string cateto2Testuale = Console.ReadLine();
-                if (double.TryParse(cateto2Testuale, out cateto2))
-                    break;
-                else
-                {
-                    Console.WriteLine("Non hai digitato un numero corretto, riprova.\n");
-                    continue;
-                }
-            }
+            double cateto1 = LeggiInputUtente("Inserisci la lunghezza del primo cateto: ");
+            double cateto2 = LeggiInputUtente("Inserisci la lunghezza del secondo cateto: ");
+
             // Teorema di Pitagora per trovare l'ipotenusa
             ipotenusa = Math.Sqrt(Math.Pow(cateto1, 2.0) + Math.Pow(cateto2, 2.0));
             // Per trovare il secondo angolo uso l'arcotangente 
